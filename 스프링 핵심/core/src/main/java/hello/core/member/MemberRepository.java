964c2d21d4d1f68807c0a57c807980e41a4f68f9
BBB <qhqo101315@gmail.com>
2024-01-22 17:53:13 +0900

    java

---
+++ 스프링 핵심/core/src/main/java/hello/core/member/MemberRepository.java
@@ -0,0 +1,11 @@
+package hello.core.member;
+
+public interface MemberRepository {
+
+    // Member 객체를 매개변수로 받아서 회원정보를 저장하는 메서드
+    void save(Member member);
+
+    // 회원 고유 식별자를 매개변수로 받아 해당 회원 정보를 조회하는 메서드
+    // 반환 타입은 Member 클래스
+    Member FindById(Long memberId);
+}
---
+++ 스프링 핵심/core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,23 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    // store 이름의 Map 객체를 선언
+    // static 으로 선언되어 클래스 레벨에서 공유되는 하나의 저장소를 나타낸다.
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member FindById(Long memberId) {
+
+        return store.get(memberId);
+    }
+}
